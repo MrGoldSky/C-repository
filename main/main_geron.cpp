4225ed5f313c6808c9c56ae030c6ef50ffb7bb9b
Ilia Rosseev <ilia19042006@gmail.com>
2023-03-19 22:47:44 +0300

    Плоащь через Герона конечный вариант

---
+++ main/main_geron.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int a, b, c;
+	cout << "Введите a b c: ";
+	cin >> a >> b >> c;
+
+	if (((a + b) < c) or ((a + c) < b) or ((b + c) < a)) {
+		cout << "Треугольника не существует";
+	}
+	else {
+		double p = (a + b + c) / 2;
+		cout << "Площадь равна = " << sqrt(p * (p - a) * (p - b) * (p - c));
+	}
+	cin >> a;
+}
+
