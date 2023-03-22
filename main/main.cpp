cec9d97001a5e308f7bc1f020f88de3bf7a56c20
Ilia Rosseev <ilia19042006@gmail.com>
2023-03-23 00:16:06 +0300

    Сортировка

--- main/main.cpp
+++ main/main.cpp
@@ -9,12 +9,22 @@ int main()
 
 	int numbers[]{ 3, -12, 5, 4, 10, 24, -1, -24, 0, 99 };
 
-	int size = 0;
-	size = end(numbers) - begin(numbers);
+	string strings[]{"а", "в", "б", "аа", "бб", "вв"};
 
-	sort(numbers, numbers + size);
+	int size_numbers = 0;
+	int size_strings = 0;
 
-	for (int i = 0; i < size; i++) {
+	size_numbers = end(numbers) - begin(numbers);
+	size_strings = end(strings) - begin(strings);
+
+	sort(numbers, numbers + size_numbers);
+	sort(strings, strings + size_strings);
+
+	for (int i = 0; i < size_numbers; i++) {
 		cout << numbers[i] << ' ';
+	}
+	cout << '\n';
+	for (int i = 0; i < size_strings; i++) {
+		cout << strings[i] << ' ';
 	}
 }
