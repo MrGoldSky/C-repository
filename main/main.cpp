650567d1fb84c35c64b572bea4b2660a6b59e7ae
Ilia Rosseev <ilia19042006@gmail.com>
2023-03-22 23:56:34 +0300

    Сортировка массива

---
+++ main/main.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	int numbers[]{ 3, -12, 5, 4, 10, 24, -1, -24, 0, 99 };
+
+	int size = 0;
+	size = end(numbers) - begin(numbers);
+
+	sort(numbers, numbers + size);
+
+	for (int i = 0; i < size; i++) {
+		cout << numbers[i] << ' ';
+	}
+}
